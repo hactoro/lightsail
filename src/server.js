7e1dd22936d863f99c4fc54158db16402f890ece
hagrand <hagrand@kbs.co.kr>
2022-04-18 14:38:10 +0900

    dotenv

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const env = require('dotenv');
 
 const port = 5003;
+env.config();
+
 
 app.get('/', (req, res)=>{
     console.log("main")
