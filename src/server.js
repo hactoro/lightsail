c2128176e6c303c93b888bf93dbd02fdb686efe6
hactoro <black1@@4>
2022-04-19 20:33:01 +0900

    test

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ console.log(path.join(__dirname, "../react-front/build"));
 
 const { PORT } = process.env;
 app.use(express.static(path.join(__dirname, "../react-front/build")));
+
+app.get("/list", (req, res)=>{
+    res.send({"message":"hello"});
+})
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "../react-front/build", "index.html"))
 })
