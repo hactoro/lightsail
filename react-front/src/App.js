d2d150f96589f113a2350b4d775192824d5af77b
hactoro <black1@@4>
2022-04-20 20:52:33 +0900

    re-build

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <video height="400" autoplay="autoplay" muted="muted" loop="loop">
-        <source src="https://thumbs.gfycat.com/FarflungCautiousHapuka-mobile.mp4" type="video/mp4" />
-      </video>
+     
     </div>
   );
 }
